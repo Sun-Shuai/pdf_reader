11526cd28da9e161efb22132eaf73c95ce10e535
sunshuai <438819781@qq.com>
2019-04-19 10:06:51 +0800

    修改为编译源码的方式使用

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,37 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        pdf_reader
-      </h1>
-      <h2 class="subtitle">
-        an online pdf reader for word segmentation translation
-      </h2>
+  <div id="app">
 
-    </div>
-  </section>
+    <iframe src="/pdf_js/viewer.html" style="width: 100%;height: 100%"></iframe>
+
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
+  export default {
+    components: {},
+    metaInfo: {},
+    data() {
+      return {}
+    }
   }
-}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  html, body {
+    height: 100%;
+    width: 100%;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  #app {
+    margin: 0 auto;
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background: #7f828b;
+  }
 </style>
