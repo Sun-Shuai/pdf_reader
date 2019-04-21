39085929a2b64c4a3bcdfdc259e15c939119cc89
sunshuai <438819781@qq.com>
2019-04-21 22:01:56 +0800

    添加ad

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <div id="app" style="padding: 0">
+  <div id="app">
 
     <iframe id="pdf" src="pdfjs/web/viewer.html" frameborder="0"></iframe>
 
+
+    <!--<div style="z-index:999;display: block; position: fixed; left: 10px; top: 300px;">-->
+
+      <!--<a href="#">-->
+        <!--<img width="320" height="300"-->
+             <!--src="qrcode.png">-->
+      <!--</a>-->
+
+    <!--</div>-->
+
   </div>
+
+
 </template>
 
 <script>
@@ -18,7 +30,7 @@
 
 <style>
 
-  html,body{
+  html, body {
     height: 100%;
     width: 100%;
   }
@@ -27,6 +39,7 @@
     height: 100%;
     width: 100%;
     position: fixed;
+    padding: 0
   }
 
   #pdf {
