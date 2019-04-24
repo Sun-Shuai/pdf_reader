6b6b58298056f8f3dab4b4b7cfdb2af2ec0325a9
sunshuai <438819781@qq.com>
2019-04-24 09:27:04 +0800

    修复轮播图

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,17 @@
     <iframe id="pdf" src="pdfjs/web/viewer.html" frameborder="0"></iframe>
 
 
-    <div style="z-index:999; position:absolute; right: 10px; bottom: 10px;">
+    <div style="z-index:999; position:absolute; right: 20px; bottom: 20px;">
 
-
-      <a href="#">
-        <img width="240" height="200" src="qrcode.png">
-      </a>
-
-
-      <el-carousel height="200px">
-        <el-carousel-item v-for="item in pics" :key="item.url">
-          <a href="#">
-            <img :width="240" :height="200" :src="item.url"/>
-          </a>
-
-        </el-carousel-item>
-      </el-carousel>
-
+      <div style="height: 200px;width: 240px">
+        <el-carousel height="200px">
+          <el-carousel-item v-for="item in pics" :key="item.url">
+            <a href="#">
+              <img :width="240" :height="200" :src="item.url"/>
+            </a>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
 
     </div>
 
@@ -39,8 +33,8 @@
       return {
         pics: [
           {url: require("../static/qrcode.png")},
-          {url: require("../static/qrcode.png")},
-          {url: require("../static/qrcode.png")}
+          // {url: require("../static/qrcode.png")},
+          // {url: require("../static/qrcode.png")}
         ]
       }
     }
